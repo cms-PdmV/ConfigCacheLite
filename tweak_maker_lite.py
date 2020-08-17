596a32486715942d478d7252879329d4a3645140
Justinas Rumsevicius <justinas.rumsevicius@gmail.com>
2020-08-17 12:59:19 +0200

    Comments and linter warnings

--- tweak_maker_lite.py
+++ tweak_maker_lite.py
@@ -1,3 +1,13 @@
+"""
+Module that has TweakMakerLite class
+
+This module is intended to be used in wmcontrol project
+in order to remove wmcontrol's dependency on WMCore
+
+wmcontrol: https://github.com/cms-PdmV/wmcontrol/
+WMCore: https://github.com/dmwm/WMCore/
+"""
+
 _TweakOutputModules = [
     "fileName",
     "logicalFileName",
@@ -72,11 +82,19 @@ _TweakParams = [
 
 
 class TweakMakerLite():
+    """
+    Stripped down version of WMCore's TweakMaker
+    Makes a dictionary with all tweaks
+    """
     def __init__(self, process_params=None, output_modules_params=None):
         self.process_level = process_params or _TweakParams
         self.output_modules_level = output_modules_params or _TweakOutputModules
 
     def make(self, process, add_parameters_list=False):
+        """
+        Make tweaks dictionary for given process
+        Optionally, add parameters_ attribute
+        """
         expanded_params = {}
         # Process parameters
         for param in self.process_level:
@@ -88,7 +106,7 @@ class TweakMakerLite():
             for param in self.output_modules_level:
                 full_path = 'process.%s.%s' % (output_module_name, param)
                 if self.has_parameter(output_module, param):
-                    expanded_params[full_path] = self.get_parameter(process, full_path)   
+                    expanded_params[full_path] = self.get_parameter(process, full_path)
 
         # Print all expanded parameters before expand dict
         # for k in sorted(expanded_params):
@@ -98,6 +116,10 @@ class TweakMakerLite():
         return expanded_dict
 
     def expand_dict(self, dictionary, add_parameters_list=False):
+        """
+        Expand flat dictinary with keys like {'a.b.c': 1} to a nested dictionary
+        such as {'a': {'b': {'c': 1}}}
+        """
         result = {}
         for key, value in dictionary.items():
             key = key.split('.')
@@ -121,6 +143,9 @@ class TweakMakerLite():
 
 
     def has_parameter(self, process, path):
+        """
+        Return whether process object has given parameter
+        """
         if isinstance(path, str):
             path = path.split('.')
             if path[0] == 'process':
@@ -135,6 +160,9 @@ class TweakMakerLite():
         return False
 
     def get_parameter(self, process, path):
+        """
+        Return a paramter from process object
+        """
         if isinstance(path, str):
             path = path.split('.')
             if path[0] == 'process':
@@ -146,6 +174,9 @@ class TweakMakerLite():
         return self.get_parameter(getattr(process, path[0]), path[1:])
 
     def expand_parameter(self, process, path, results, full_path=''):
+        """
+        Fill results dictionary with existing parameter paths and values
+        """
         if isinstance(path, str):
             path = path.split('.')
             if path[0] == 'process':
@@ -168,4 +199,3 @@ class TweakMakerLite():
                                       path[1:],
                                       results,
                                       next_path)
-
